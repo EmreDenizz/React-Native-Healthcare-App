306a1fd680532efb974361d9da032be2df4eef3c
Emre Deniz <emre.deniz@outlook.com>
2023-10-23 14:59:23 -0400

    Create package.json, gitignore and style.js

---
+++ styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+  
+export const Colors = {                                      
+    ligt: '#f0f0f0',
+    dark: '#080909'
+};
+  
+const baseContainerStyles = {                                
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+};
