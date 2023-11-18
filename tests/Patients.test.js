00b352a00875e88a245bc9e811afeff7703277e0
Emre Deniz <emre.deniz@outlook.com>
2023-11-17 22:05:28 -0500

    Unit tests for Patients screen

---
+++ tests/Patients.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Patients from '../screens/Patients';
+
+// Render
+const tree = renderer.create(<Patients />).toJSON();
+
+// Check component to have one child element
+describe('<Patients />', () => {
+    it('has 1 child', () => {
+        expect(tree.children.length).toBe(1);
+    });
+});
+
+// Check screen has the logo image
+describe('<Patients Image Logo />', () => {
+    it('has the logo image on screen image', () => {
+        expect(tree.children[0].children[0].children[0].children[0].children[0].type).toBe("Image");
+    });
+});
